da54dce8f5c0a87ee297147eb397486927cad0f6
Katya Rozhko <49344755+KatyaRzh@users.noreply.github.com>
2021-11-26 16:56:19 -0800

    fixed bug

--- src/Results.js
+++ src/Results.js
@@ -4,16 +4,17 @@ import Meaning from "./Meaning";
 export default function Results(props) {
 	if (props.results) {
 		return (
-		<div className="Results">
-			<h2>{props.results.word}</h2>
-			{props.results.meanings.map(function (meaning, index) {
-				return (
-					<div key={index}>
-						<Meaning meaning={meaning} />
-					</div>
-				);
-			})}
-		</div>
+			<div className="Results">
+				<h2>{props.results.word}</h2>
+				{props.results.meanings.map(function (meaning, index) {
+					return (
+						<div key={index}>
+							<Meaning meaning={meaning} />
+						</div>
+					);
+				})}
+			</div>
+		);
 	} else {
 		return null;
 	}
