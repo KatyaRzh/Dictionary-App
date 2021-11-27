091654644e612cb04f16c6a770db8df9bddf4b2a
Katya Rozhko <49344755+KatyaRzh@users.noreply.github.com>
2021-11-26 17:22:36 -0800

    Photos, phonetic component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,22 +1,40 @@
 import React, { useState } from "react";
-import "./Dictionary.css";
 import axios from "axios";
-import Results from "./Results.js";
+import Results from "./Results";
+import Photos from "./Photos";
+import "./Dictionary.css";
 
-export default function Dictionary() {
-	let [keyword, setKeyword] = useState("");
-	let [results, setResults] = useState({});
+export default function Dictionary(props) {
+	let [keyword, setKeyword] = useState(props.defaultKeyword);
+	let [results, setResults] = useState(null);
+	let [loaded, setLoaded] = useState(false);
+	let [photos, setPhotos] = useState(null);
 
-	function handleResponse(response) {
+	function handleDictionResponse(response) {
 		setResults(response.data[0]);
 	}
 
-	function search(event) {
-		event.preventDefault();
+	function handlePexelsResponse(response) {
+		setPhotos(response.data.photos);
+	}
 
-		// documentation https://dictionaryapi.dev/
+	function search() {
+		// documentation: https://dictionaryapi.dev/e
 		let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-		axios.get(apiUrl).then(handleResponse);
+		axios.get(apiUrl).then(handleDictionResponse);
+
+		let pexelsApiKey =
+			"563492ad6f91700001000001fdd29f0808df42bd90c33f42e128fa89";
+		let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+		let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+		axios
+			.get(pexelsApiUrl, { headers: headers })
+			.then(handlePexelsResponse);
+	}
+
+	function handleSubmit(event) {
+		event.preventDefault();
+		search();
 	}
 
 	function handleKeywordChange(event) {
