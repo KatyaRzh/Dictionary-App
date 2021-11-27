235af48e8127e1c5ceca94c138be9d63756541d7
Katya Rozhko <49344755+KatyaRzh@users.noreply.github.com>
2021-11-26 16:46:18 -0800

    adding word and meaning/definition on the page

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
+import Results from "./Results.js";
 
 export default function Dictionary() {
 	let [keyword, setKeyword] = useState("");
+	let [results, setResults] = useState({});
 
 	function handleResponse(response) {
-		console.log(response.data[0]);
+		setResults(response.data[0]);
+		// console.log(response.data[0].meanings[0].definition[0].definition);
 	}
 	// documentation https://dictionaryapi.dev/
 	function search(event) {
@@ -27,6 +30,7 @@ export default function Dictionary() {
 			<form onSubmit={search}>
 				<input type="search" onChange={handleKeywordChange} />
 			</form>
+			<Results results={results} />
 		</div>
 	);
 }
